83924154076d2873742c630f4c5145da39096d40
Geonwoo Park <parkkeonwo@naver.com>
2022-06-22 18:14:08 +0900

    220622 18:14

--- practice/practice.py
+++ practice/practice.py
@@ -1,3 +1,5 @@
+####################### 한손 ########################3
+
 # import cv2
 # import mediapipe as mp
 # import numpy as np
@@ -105,7 +107,7 @@
 
 
 
-
+################################# 두손 #######################################3
 # import cv2
 # import mediapipe as mp
 # import numpy as np
@@ -215,6 +217,8 @@
 #         break
 
 
+
+############################ cam 2개 키기 ######################
 # import cv2
 # import threading
 
@@ -251,213 +255,197 @@
 
 
 
+####################### hand detection #########################
 
-# import threading
-# from queue import Queue
 # import cv2
 # import mediapipe as mp
+# mp_drawing = mp.solutions.drawing_utils
+# mp_drawing_styles = mp.solutions.drawing_styles
+# mp_hands = mp.solutions.hands
+
+# cap = cv2.VideoCapture(1)
+# width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+# height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+# # cap.set(cv2.CAP_PROP_FRAME_WIDTH,2048)
+# # cap.set(cv2.CAP_PROP_FRAME_HEIGHT,1152)
+
+# with mp_hands.Hands(
+#     model_complexity=0,
+#     max_num_hands=1,
+#     min_detection_confidence=0.5,
+#     min_tracking_confidence=0.5) as hands:
+#   while cap.isOpened():
+#     success, image = cap.read()
+#     # image = cv2.resize(image, (2048,1152))
+#     image = cv2.resize(image, (2560,1440), interpolation=cv2.INTER_LANCZOS4)
+#     # image = cv2.resize(image, (3072,1728))
+#     if not success:
+#       print("Ignoring empty camera frame.")
+#       continue
+
+#     image.flags.writeable = False
+#     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#     results = hands.process(image)
+
+#     image.flags.writeable = True
+#     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+#     if results.multi_hand_landmarks:
+#       for hand_landmarks in results.multi_hand_landmarks:
+#         mp_drawing.draw_landmarks(
+#             image,
+#             hand_landmarks,
+#             mp_hands.HAND_CONNECTIONS,
+#             mp_drawing_styles.get_default_hand_landmarks_style(),
+#             mp_drawing_styles.get_default_hand_connections_style())
+        
+#         # xlist = []
+#         # ylist = []
+#         # landmark_list = []
+#         # for id, lm in enumerate(hand_landmarks.landmark):
+#         #   x,y = lm.x*width, lm.y*height
+#         #   landmark_list.append([x,y])
+
+#     cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+#     if cv2.waitKey(5) & 0xFF == 27:
+#       break
+# cap.release()
+
+
+
+######### 이미지 불러오기 ############3
+
+# import cv2
+
+# img = cv2.imread("D:/ai-festival/practice/image/2022-06-22-11-38-46.jpg")
+# print(img)    # numpy array
+
+
+# cv2.imshow("image", img)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+
+
+############# 한글 적기 ################
+
+
+# from PIL import ImageFont, ImageDraw, Image
+# import cv2
 # import numpy as np
 
-# class camThread(threading.Thread):
-#     def __init__(self, previewName, camID):
-#         threading.Thread.__init__(self)
-#         self.previewName = previewName
-#         self.camID = camID
-#     def run(self):
-#         print("Starting " + self.previewName)
-#         camPreview(self.previewName, self.camID)
+# font = ImageFont.truetype('D:/ai-festival/practice/fonts/SCDream6.otf',30)
 
-# def camPreview(previewName, camID):
+# cap = cv2.VideoCapture(0)
 
-#     cv2.namedWindow(previewName)
-#     cap = cv2.VideoCapture(camID)
+# while(True):
+#     ret, frame = cap.read()    # Read 결과와 frame
+#     if(ret) :
 
-#     while cap.isOpened():   
-#         ret, img = cap.read() 
-#         if not ret:     
-#             continue     
+#         frame = Image.fromarray(frame)
+#         draw = ImageDraw.Draw(frame)
+#         draw.text( xy=(30,20), text="V를 하면 사진이 찍힙니다.", font=font, fill=(0,0,0)  )
+#         frame = np.array(frame)
 
-#         img = cv2.flip(img, 1)
+#         cv2.imshow('Window', frame)
 
-#         cv2.imshow(previewName, img)
-
-#         if cv2.waitKey(5) & 0xFF == 27:
+#         if cv2.waitKey(10) == ord('q'):
 #             break
 
+# cap.release()
+# cv2.destroyAllWindows()
 
-# def cam(previewName, camID,q):
 
-#     cv2.namedWindow(previewName)
-#     cap = cv2.VideoCapture(camID)
 
-#     while cap.isOpened():   
-#         ret, img = cap.read() 
-#         if not ret:     
-#             continue     
 
-#         img = cv2.flip(img, 1)
+################ 이미지 합성하기 ################
 
-#         item = 1
-#         evt = threading.Event()
-#         q.put((item, evt))
-#         evt.wait()
 
-#         cv2.imshow(previewName, img)
+# from PIL import ImageFont, ImageDraw, Image
+# import cv2
+# import numpy as np
 
-#         if cv2.waitKey(5) & 0xFF == 27:
-#             break
+# font = ImageFont.truetype('D:/ai-festival/practice/fonts/SCDream6.otf',30)
 
-import threading
-from queue import Queue
-import cv2
-import mediapipe as mp
-import numpy as np
-import datetime
-from PIL import ImageFont, ImageDraw, Image
-import time
-from collections import deque
+# cap = cv2.VideoCapture(0)
+# width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+# height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-def cam(previewName, camID,q1,q2):
+# while(True):
+#     ret, img = cap.read()    # Read 결과와 img
+#     if(ret) :
 
-    # font
-    # font = ImageFont.truetype('./fonts/SCDream6.otf',20)
-    font = ImageFont.truetype('D:/ai-festival/practice/fonts/SCDream6.otf',30)
+#         img = Image.fromarray(img)
+#         draw = ImageDraw.Draw(img)
+#         draw.text( xy=(30,20), text="V를 하면 사진이 찍힙니다.", font=font, fill=(0,0,0)  )
+#         img = np.array(img)
 
-    max_num_hands = 1
-    rps_gesture = {0:'rock', 5:'paper', 9:'scissors'}      # 가위바위보를 하기 위해 0,5,9번을 새롭게 정의
+#         upload_image = cv2.imread('D:/ai-festival/practice/save_image/after2.png')
+#         rows, cols, channels = upload_image.shape
+#         roi = img[50:rows+50,50:cols+50]
 
-    mp_hands = mp.solutions.hands        
-    mp_drawing = mp.solutions.drawing_utils
-
-    hands = mp_hands.Hands(               
-        max_num_hands=max_num_hands,       
-        min_detection_confidence=0.5,        
-        min_tracking_confidence=0.5)
-
-    # Gesture recognition model  # 제스쳐 인식 모델
-    file = np.genfromtxt('D:/ai-festival/practice/data/gesture_train.csv', delimiter=',')    # data에 gesture_train.csv파일이 있다. 각각의 제스쳐/각도/라벨 저장되어있음
-    # file = np.genfromtxt('./data/gesture_train.csv', delimiter=',')
-
-    angle = file[:,:-1].astype(np.float32)        # 앵글 데이터를 모아줌
-    label = file[:, -1].astype(np.float32)        # 라벨 데이터를 모아줌
-    knn = cv2.ml.KNearest_create()                # opencv의 k-nearst-neighbors 알고리즘 사용하여
-    knn.train(angle, cv2.ml.ROW_SAMPLE, label)      # 학습을 시켜버림
-
-    cv2.namedWindow(previewName)
-    cap = cv2.VideoCapture(camID)
-
-    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-
-    while cap.isOpened():   
-        now = datetime.datetime.now()
-        nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
-        nowDatetime_path = now.strftime('%Y-%m-%d %H_%M_%S')      # 파일이름으로는 :를 못쓰기 때문에 따로
-
-        ret, img = cap.read() 
-        if not ret:     
-            continue     
-
-        img = cv2.flip(img, 1)
-
-        resize_width, resize_height = 640, 480
-        img = cv2.resize(img, (resize_width,resize_height))
-
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-        result = hands.process(img)
-
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-
-        if result.multi_hand_landmarks is not None:   
-            for res in result.multi_hand_landmarks:   
-                joint = np.zeros((21, 3))      
-                for j, lm in enumerate(res.landmark):   
-                    joint[j] = [lm.x, lm.y, lm.z]     
-
-                # 각 joint로 벡터를 계산해서 각도를 계산 -> 각각 관절(joint와 joint사이)에 대한 벡터를 구해줌 
-                v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19],:] # Parent joint
-                v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],:] # Child joint
-                v = v2 - v1 # [20,3]
-                # 벡터를 각각 길이로 나눠줘서 normalize 해줌 --> 단위벡터(크기 1)
-                v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
-
-                # 벡터의 내적을 이용해서 각도를 구해준다. 크기가 1이므로 각각의 내적값은 두벡터가 이루는 cos값이 되고
-                # 이를 역함수인 arccos를 넣어주면 각각이 이루는 각을 계산해준다.
-                angle = np.arccos(np.einsum('nt,nt->n',
-                    v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:], 
-                    v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:])) # [15,]  # 15개의 각도를 구해서 angle변수에 저장
-
-                angle = np.degrees(angle) # Convert radian to degree # 라디안 값을 각으로 변형
-
-                # 아까 제스쳐모델을 학습시켰는데, 학습시킨 knn 모델을 가져다가 inference를 진행한다.  # Inference gesture
-                data = np.array([angle], dtype=np.float32) # numpy array로 바꿔주고, float32비트로 형태로 바꿔주고 
-                ret, results, neighbours, dist = knn.findNearest(data, 3)     # k가 3일때의 값을 구해줌
-                idx = int(results[0][0])    # 결과는 result의 첫번째 index에 저장
-
-                # Draw gesture result
-                # if idx in rps_gesture.keys():    # 만약에 인덱스가 가위바위보중에 하나라면,
-                #     cv2.putText(img, text=rps_gesture[idx].upper(), org=(int(res.landmark[0].x * img.shape[1]), int(res.landmark[0].y * img.shape[0] + 20)), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
-                    # 가위바위보 결과를 표시한다.  # opencv의 putText를 사용해서 가위인지,바위인지,보인지 글씨로 쓰도록 함. 
-
-                if idx == 9:
-                    q1.put((nowDatetime_path))
-
-                # mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)  # 손가락 마디마디에 랜드마크를 그리는 함수를 미디어 파이프의 drawrandmark 함수를 이용해서 그린다.
-
-        ### 한글 넣기
-        if q2.qsize() <= 3:
-            img = Image.fromarray(img)
-            draw = ImageDraw.Draw(img)
-            draw.text( xy=(30,20), text="V를 하면 사진이 찍힙니다.", font=font, fill=(255,255,255)  )
-            img = np.array(img)
-
-        text_color = (0,255,255)
-        if q2.qsize() == 1:
-            cnt = 3
-            cv2.putText(img, text=str(cnt), org=(int(width*1/2),int(height*1/2)),
-                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=4, color=text_color, thickness=5)
-        if q2.qsize() == 2:
-            cnt = 2
-            cv2.putText(img, text=str(cnt), org=(int(width*1/2),int(height*1/2)),
-                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=4, color=text_color, thickness=5)
-        if q2.qsize() == 3:
-            cnt = 1
-            cv2.putText(img, text=str(cnt), org=(int(width*1/2),int(height*1/2)),
-                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=4, color=text_color, thickness=5)
-        if q2.qsize() == 4:
-            # cv2.imwrite("./image/"+nowDatetime_path+".jpg", img)
-            cv2.imwrite("D:/ai-festival/practice/image/"+nowDatetime_path+".jpg", img)
-            cv2.rectangle(img, (0,0),(resize_width,resize_height),(255,255,255),-1)
+#         gray = cv2.cvtColor(upload_image, cv2.COLOR_BGR2GRAY) #로고파일의 색상을 그레이로 변경
+#         ret, mask = cv2.threshold(gray, 160, 255, cv2.THRESH_BINARY) #배경은 흰색으로, 그림을 검정색으로 변경
+#         mask_inv = cv2.bitwise_not(mask)
+#         # cv2.imshow('mask',mask) #배경 흰색, 로고 검정
+#         # cv2.imshow('mask_inv',mask_inv) # 배경 검정, 로고 흰색
         
+#         src1_bg = cv2.bitwise_and(roi,roi,mask=mask) #배경에서만 연산 = src1 배경 복사
+#         # cv2.imshow('src1_bg',src1_bg)
+        
+#         src2_fg = cv2.bitwise_and(upload_image, upload_image, mask = mask_inv) #로고에서만 연산
+#         # cv2.imshow('src2_fg',src2_fg)
+        
+#         dst = cv2.bitwise_or(src1_bg, src2_fg) #src1_bg와 src2_fg를 합성
+#         # cv2.imshow('dst',dst)
+        
+#         img[50:rows+50,50:cols+50] = dst #src1에 dst값 합성
 
-        cv2.imshow(previewName, img)
+#         cv2.imshow('Window', img)
+
+#         if cv2.waitKey(5) & 0xFF == 27:
+#             break
+
+# cap.release()
+# cv2.destroyAllWindows()
+
+
+
+
+
+
+from PIL import ImageFont, ImageDraw, Image
+import cv2
+import numpy as np
+import time
+
+font = ImageFont.truetype('D:/ai-festival/practice/fonts/SCDream6.otf',30)
+
+cap = cv2.VideoCapture(0)
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+while(True):
+    ret, img = cap.read()    # Read 결과와 img
+    if(ret) :
+
+        img = Image.fromarray(img)
+        draw = ImageDraw.Draw(img)
+        draw.text( xy=(30,20), text="V를 하면 사진이 찍힙니다.", font=font, fill=(0,0,0)  )
+        img = np.array(img)
+
+        upload_image = cv2.imread('D:/ai-festival/practice/save_image/after.png')
+        img = cv2.addWeighted(img,0,upload_image,1,0)
+
+        time.sleep(3)
+        
+        img = cv2.addWeighted(img,1,upload_image,0,0)
+
+        cv2.imshow('Window', img)
 
         if cv2.waitKey(5) & 0xFF == 27:
             break
 
-
-def handler(q1,q2):
-    while True:
-        print()     # print 빼면 오류 걸림
-        if q1.qsize() != 0:
-            for i in range(4):
-                q2.put((3-i))
-                time.sleep(1)
-
-if __name__ == '__main__':
-    q1 = Queue()
-    q2 = Queue()
-
-    # deque_data = []
-    # dq1 = deque(deque_data, maxlen=10)
-    # thread1 = camThread("Camera 0", 0)
-    thread1 = threading.Thread(target=handler, args=(q1,q2,))
-    thread2 = threading.Thread(target=cam, args=("Camera 0",0,q1,q2,))
-
-    thread1.start()
-    thread2.start()
-
+cap.release()
+cv2.destroyAllWindows()
 
 
 
